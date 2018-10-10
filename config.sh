de03290fd459690df36ffceeb1adee55f8931801
EvheniiZotkin <zotkin.isq@gmail.com>
2018-10-10 13:59:44 +0200

    add images

--- config.sh
+++ config.sh
@@ -1,11 +1,11 @@
 
 apt-get install tk-dev python-tk
-pip install cffi
-pip install pandas
-pip install pycocotools
-pip install cython
-pip install opencv-python
-pip install requests
+pip3 install cffi
+pip3 install pandas
+pip3 install pycocotools
+pip3 install cython
+pip3 install opencv-python
+pip3 install requests
 bash lib/build.sh
 cd ../
 python train.py --dataset csv --csv_train data/annotations.csv --csv_classes data/classes.csv --csv_val data/annotations.csv --epochs 10
