a06b7fdac3895e90c9f6f28053bad128ddc44624
EvheniiZotkin <zotkin.isq@gmail.com>
2018-10-13 13:02:38 +0200

    2 images

--- config.sh
+++ config.sh
@@ -1,12 +1,13 @@
-
 apt-get install tk-dev python-tk
-pip3 install cffi
-pip3 install pandas
-pip3 install pycocotools
-pip3 install cython
-pip3 install opencv-python
-pip3 install requests
+pip install cffi
+pip install pandas
+pip install pycocotools
+pip install cython
+pip install opencv-python
+pip install requests
 bash lib/build.sh
-cd ../
-python train.py --dataset csv --csv_train data/annotations.csv --csv_classes data/classes.csv --csv_val data/annotations.csv --epochs 10
+cd ..
 
+
+python visualize.py --dataset csv --csv_classes data/cla.csv  --csv_val <path/to/val_annots.csv> --model <path/to/model.pt>
+python3 visualize.py --dataset csv  --csv_classes data/classes.csv --csv_val data/annotations.csv --model model_final.pt 
